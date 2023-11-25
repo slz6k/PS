a764c88dd3861ae73987f891ac7423ad5d2c3ecb
민경 <tjalsrud0101@gmail.com>
2023-11-25 23:54:01 +0900

    스택 2

---
+++ baekjoon/28278.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+
+    static Stack<Integer> stack = new Stack<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); //명령어의 수
+
+        while(N --> 0){
+            solution(br.readLine());
+        }
+
+        br.close();
+
+        System.out.println(sb);
+    }
+
+    static void solution(String input){
+        char c = input.charAt(0);
+
+        switch (c){
+            case '1' : stack.push(Integer.parseInt(input.substring(2))); return;
+            case '2' : sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n"); return;
+            case '3' : sb.append(stack.size()).append("\n"); return;
+            case '4' : sb.append(stack.isEmpty() ? 1 : 0).append("\n"); return;
+            case '5' : sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n"); return;
+            default: break;
+        }
+    }
+}
