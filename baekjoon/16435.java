858e7bc0088962665707e8392e6b1f09fef4365a
민경 <tjalsrud0101@gmail.com>
2024-02-16 11:29:40 +0900

    스네이크버드

---
+++ baekjoon/16435.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] fruits = new int[N];
+        for(int i = 0; i < N; i++) {
+            fruits[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(fruits);
+
+        for(int fruit : fruits) {
+            if(fruit <= L) {
+                L++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(L);
+    }
+}
