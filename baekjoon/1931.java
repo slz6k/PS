8c13b99b568c16ca7b1b2ef882afcb3420e2a430
민경 <tjalsrud0101@gmail.com>
2024-04-25 23:27:07 +0900

    회의실 배정

---
+++ baekjoon/1931.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] times = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            times[i][0] = Integer.parseInt(st.nextToken());
+            times[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(times, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+
+        });
+
+        int count = 0;
+        int prev = 0;
+        for(int i = 0; i < N; i++) {
+            if(prev <= times[i][0]) {
+                prev = times[i][1];
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+}
