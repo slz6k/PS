13c9f2e0c50de41fd8a73aa739b2b605419cd4a8
민경 <tjalsrud0101@gmail.com>
2024-06-07 14:00:06 +0900

    창문 닫기

---
+++ baekjoon/13909.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = (int) Math.sqrt(N);
+        System.out.print(count);
+    }
+}
