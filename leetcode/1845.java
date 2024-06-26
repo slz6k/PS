a33b3e344be6b47b734d29db9c30a6d54ae039ba
민경 <tjalsrud0101@gmail.com>
2024-06-26 23:22:01 +0900

    Seat Reservation Manager

---
+++ leetcode/1845.java
@@ -0,0 +1,18 @@
+class SeatManager {
+    PriorityQueue<Integer> minHeap;
+
+    public SeatManager(int n) {
+        minHeap = new PriorityQueue<>();
+        for(int i = 1; i <= n; i++) {
+            minHeap.add(i);
+        }
+    }
+
+    public int reserve() {
+        return minHeap.poll();
+    }
+
+    public void unreserve(int seatNumber) {
+        minHeap.add(seatNumber);
+    }
+}
