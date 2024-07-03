8dc66aae6e83503fc487af42e8aa85ca5d964d93
민경 <tjalsrud0101@gmail.com>
2024-07-03 21:02:45 +0900

    팩토리얼 2

--- baekjoon/27433.java
+++ baekjoon/27433.java
@@ -5,14 +5,14 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        System.out.println(fibo(N));
+        System.out.println(factorial(N));
     }
 
-    public static long fibo(int N) {
+    public static long factorial(int N) {
         if(N == 0 || N == 1) {
             return 1;
         }
 
-        return fibo(N-1) * N;
+        return factorial(N-1) * N;
     }
 }
