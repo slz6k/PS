91e393a3d75f5114fab871e08621fea585e4b6bc
민경 <tjalsrud0101@gmail.com>
2024-07-11 11:28:07 +0900

    약수

---
+++ baekjoon/1037.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        System.out.println(nums[0] * nums[n-1]);
+    }
+}
