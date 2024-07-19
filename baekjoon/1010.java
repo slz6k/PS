1ba73ad30962c948ba73d7b3ff7d519f4feee9e7
민경 <tjalsrud0101@gmail.com>
2024-07-20 00:04:23 +0900

    다리 놓기

--- baekjoon/1010.java
+++ baekjoon/1010.java
@@ -7,19 +7,24 @@ class Main {
         int t = Integer.parseInt(br.readLine());
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+
+        long[][] dp = new long[31][31];
+        for(int i = 1; i <= 30; i++) {
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+
+        for(int i = 2; i <= 30; i++) {
+            for(int j = 1; j < i; j++) {
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }
+        }
+
         while(t-- > 0) {
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-            long a = 1;
-            long b = 1;
-            for(int i = 0; i < n; i++) {
-                a *= (m-i);
-            }
-            for(int i = n; i > 0; i--) {
-                b *= i;
-            }
-            sb.append(a/b).append("\n");
+            sb.append(dp[m][n]).append("\n");
         }
         System.out.print(sb);
     }
