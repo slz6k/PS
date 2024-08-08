d9726050022294b655d5442ae912facb31882967
민경 <tjalsrud0101@gmail.com>
2024-08-08 16:24:45 +0900

    N과 M (2)

---
+++ baekjoon/15650.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int[] nums;
+    static boolean[] visited;
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        nums = new int[m];
+        visited = new boolean[n];
+        dfs(0, 0);
+
+        System.out.print(sb);
+    }
+
+    public static void dfs(int start, int depth) {
+        if(depth == m) {
+            for(int num : nums) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                nums[depth] = i + 1;
+                dfs(i + 1, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
