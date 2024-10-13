2569eec7417d9a49ea27f6c4d10f5681a7270643
민경 <tjalsrud0101@gmail.com>
2024-10-13 19:28:13 +0900

    N번째 큰 수

---
+++ baekjoon/2075.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for(int i = 1; i < N; i++) {
+            pq.poll();
+        }
+
+        System.out.print(pq.poll());
+    }
+}
